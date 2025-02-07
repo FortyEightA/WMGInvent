baaedf5b752c39920c200d45324f0399c753968b
FortyEightA <iggyniznik@gmail.com>
2025-02-07 23:47:15 +0000

    Session system for login
    Began creating an account system using session and eventually SQlite3 atabase

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
 views = Blueprint('views', __name__, static_folder='static',
                   template_folder='templates')
@@ -8,3 +8,24 @@ views = Blueprint('views', __name__, static_folder='static',
 @views.route('/home')
 def home():
     return render_template('home.html')
+
+
+@views.route('/account')
+def account():
+    return render_template('account.html')
+
+
+@views.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('views.account'))
+
+
+@views.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        session['username'] = username
+        return redirect(url_for('views.account'))
+    else:
+        return render_template('login.html')
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app import templates, static
 from app.views import views
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.secret_key = "WMGInvent"
 app.register_blueprint(views, url_prefix='')
 
 if __name__ == '__main__':
