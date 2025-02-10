fde952787df4afcaaa8953c9e93aec58fc84ba8f
FortyEightA <iggyniznik@gmail.com>
2025-02-10 00:30:32 +0000

    Added Update function to accounts
    Added new page to update account information

--- app/views.py
+++ app/views.py
@@ -7,6 +7,12 @@ conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 cursor.execute(
     "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS cars(id INTEGER PRIMARY KEY, make TEXT, model TEXT, year INTEGER, registration TEXT, status TEXT)"
+)
+cursor.execute(
+    "Create table if not exists changes(id INTEGER PRIMARY KEY, car_id INTEGER, change TEXT, date TEXT)"
+)
 cursor.close()
 conn.close()
 
@@ -49,6 +55,34 @@ def register():
             return redirect(url_for('views.login'))
     else:
         return render_template('register.html')
+
+
+@views.route('/update', methods=["POST", "GET"])
+def update():
+    if request.method == "POST":
+        username = session['username']
+        password = session['password']
+        session.pop('username', None)
+        session.pop('password', None)
+        new_username = request.form['new_username']
+        new_password = request.form['new_password']
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        if cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password)).fetchone():
+            cursor.execute("UPDATE users SET username=?, password=? WHERE username=? AND password=?",
+                           (new_username, new_password, username, password))
+            session['username'] = new_username
+            session['password'] = new_password
+            conn.commit()
+            cursor.close()
+            conn.close()
+            flash('User updated successfully')
+            return redirect(url_for('views.account'))
+        else:
+            flash('Invalid username or password')
+            return render_template('update.html')
+    else:
+        return render_template('update.html')
 
 
 @views.route('/delete')
--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.views import views
+from datetime import timedelta
 
 app = Flask(__name__, static_folder='app/static',
             template_folder='app/templates')
 app.secret_key = "WMGInvent"
+app.permanent_session_lifetime = timedelta(minutes=5)
 app.register_blueprint(views, url_prefix='')
 
 
